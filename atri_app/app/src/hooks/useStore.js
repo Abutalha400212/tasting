4d1ddf8819606cb7b790c0cf7759112bb4f55e60
Abu Talha <abutalha141999@gmail.com>
2022-12-24 15:36:24 +0600

    deploy

--- atri_app/app/src/hooks/useStore.js
+++ atri_app/app/src/hooks/useStore.js
@@ -42,18 +42,7 @@ const desktopModeProps = {
   "Home": {
     "bar1": {
       "custom": {
-        "data": [
-          {
-            "x": 2010,
-            "category1": 40,
-            "category2": 50
-          },
-          {
-            "x": 2011,
-            "category1": 30,
-            "category2": 60
-          }
-        ],
+        "data": [],
         "xAxis": {
           "show": true,
           "key": "x"
